cfe99e80a464b66ec12e18e6569947acce90358c
Jonas Lindell <Carljonaslindell@gmail.com>
2021-09-20 14:17:31 +0200

    db connection and output to db

--- src/backend/RestAPI.cs
+++ src/backend/RestAPI.cs
@@ -14,7 +14,9 @@ namespace CosmosDB_RestAPI
     {
         [FunctionName("RestAPI")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, [CosmosDB(databaseName: "my-database", collectionName: "my-container",
+            ConnectionStringSetting = "CosmosDbConnectionString"
+            )]IAsyncCollector<dynamic> documentsOut,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -24,11 +26,22 @@ namespace CosmosDB_RestAPI
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+            
+            if (!string.IsNullOrEmpty(name))
+            {
+                // Add a JSON document to the output container.
+                await documentsOut.AddAsync(new
+                {
+                    // create a random ID
+                    id = System.Guid.NewGuid().ToString(),
+                    name = name
+                });
+            }
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
+    
             return new OkObjectResult(responseMessage);
         }
     }
