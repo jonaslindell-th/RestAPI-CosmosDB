6097924ec5e6c8a84afef83155f3fc1119ff956c
Jonas Lindell <Carljonaslindell@gmail.com>
2021-09-21 12:38:44 +0200

    Update ToDoGet.cs

--- src/backend/ToDoGet.cs
+++ src/backend/ToDoGet.cs
@@ -7,31 +7,40 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
-namespace ToDoGet
+namespace CosmosDB_RestAPI
 {
     public static class ToDoGet
     {
         [FunctionName("ToDoGet")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, [CosmosDB(databaseName: "my-database", collectionName: "my-container",
-            ConnectionStringSetting = "CosmosDbConnectionString"
-            )]IAsyncCollector<dynamic> documentsOut,
+            ConnectionStringSetting = "CosmosDbConnectionString",
+            SqlQuery ="SELECT * FROM c"
+            )]IEnumerable<ToDoModel> todos,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            if (todos == null)
+            {
+                return new NotFoundResult();
+            }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            return new OkObjectResult(todos);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully. Wohooo.";
+            // string name = req.Query["name"];
 
-            return new OkObjectResult(responseMessage);
+            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            // dynamic data = JsonConvert.DeserializeObject(requestBody);
+            // name = name ?? data?.name;
+
+            // string responseMessage = string.IsNullOrEmpty(name)
+            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //     : $"Hello, {name}. This HTTP triggered function executed successfully. Wohooo.";
+
+            // return new OkObjectResult(responseMessage);
         }
     }
 }
